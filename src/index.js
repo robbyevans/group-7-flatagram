e4a3107f64bfc52d091079cf1ecf706a75b4662c
karugo-murage <murage.gidion@moringaschool.com>
2022-06-22 11:20:48 +0300

    add for loop

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ function fetchComments(){
     fetch('http://localhost:3000/comments')
     .then((resp)=>resp.json())
     .then((data)=>{
+        for(let item of data){
+            const ul=document.querySelector("#comments-list")
+            const li=document.createElement('li')
+            li.innerHTML=item.content
+            ul.appendChild(li)
+            
+          }
+        })
+      }
+
